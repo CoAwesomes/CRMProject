80345ecb5106532fe6d992e2763c68bc5d3659b6
ghtpdkzz <ghtpdkzz@naver.com>
2018-02-09 00:01:47 +0900

    add react toy

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './header/Header';
+import Body from './body/Body';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
+	render() {
+		return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
+        <Body name="Sara" onClick="asdf()" date={new Date()}/>
       </div>
-    );
-  }
+		);
+	}
 }
 
 export default App;
+
